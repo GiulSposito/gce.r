9e9473a6c9d71860731e65ee81581dde66e12954
Giuliano Sposito <gsposito@ciandt.com>
2017-04-22 20:09:56 -0300

    not working!

--- R/cge_cluster_test.R
+++ R/cge_cluster_test.R
@@ -24,13 +24,19 @@ system.time(boot <- foreach(i = 1:100, .packages = "boot") %dopar% { run() })
 ##### CGE cluster
 library(googleComputeEngineR)
 
-vms <- lapply(paste0("gcer-node-", 1:5), FUN = googleComputeEngineR::gce_vm, template = "henrikbengtsson/r-base-future")
+vms <- lapply(paste0("gcer-node-", 1:5), FUN = googleComputeEngineR::gce_vm, template = "r-base")
 
 vms <- lapply(vms, FUN = gce_ssh_setup, 
               key.pub = file.path("C:/Users/gsposito/.ssh", "google_compute_engine.pub"),
               key.private = file.path("C:/Users/gsposito/.ssh", "google_compute_engine"))
 
-cl <- as.cluster(vms) #, docker_image = "henrikbengtsson/r-base-future")
+vms <- lapply(vms,
+              gce_future_install_packages,
+              docker_image = "henrikbengtsson/r-base-future",
+              cran_packages = c("doFuture","Future","googleComputeEngineR"))
+              
+
+cl <- as.cluster(vms, docker_image = "henrikbengtsson/r-base-future")
 plan(cluster, workers = cl)
 system.time(boot <- foreach(i = 1:100, .packages = "boot") %dopar% { run() })
 
@@ -38,3 +44,11 @@ lapply(vms, gce_vm_stop)
 
 
 lapply(vms, gce_vm_delete)
+
+
+plan(multiprocess)
+
+result <- foreach(i=2:5, .packages = c("googleComputeEngineR")) %dopar% { 
+  gce_future_install_packages(vms[[i]],docker_image = "henrikbengtsson/r-base-future",
+                              cran_packages = c("doFuture","Future","googleComputeEngineR"))
+}
